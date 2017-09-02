06f593a46ac4d8f959cb6e51c30ddf16b3cdb360
ojall <ousainouj@gmail.com>
2017-09-02 14:08:01 -0400

    2_14

---
+++ src/Assignment1/Chapter2_14.java
@@ -0,0 +1,25 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+/*
+ * Write a program that prompts the user to enter a weight in pounds and height in inches and displays the BMI.
+ */
+public class Chapter2_14 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter wieght in pounds");
+		Scanner input = new Scanner(System.in);
+		double weightInPounds = input.nextDouble();
+		System.out.println("Enter height in inches");
+		double heightInInches = input.nextDouble();
+		
+		double weightInKilos = weightInPounds * 0.45359237;
+		double heightInMeters = heightInInches * 0.0254;
+		double heightSquared = Math.pow(heightInMeters, 2);
+		double BMI = (weightInKilos) / (heightSquared);
+		System.out.println("BMI is: " + BMI);
+
+	}
+
+}
