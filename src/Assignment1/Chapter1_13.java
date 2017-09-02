1eb52aac5313d9a8b0d42457a12ad78b9d98e561
ojall <ousainouj@gmail.com>
2017-09-02 04:08:32 -0400

    1_13 Completed

---
+++ src/Assignment1/Chapter1_13.java
@@ -0,0 +1,26 @@
+package Assignment1;
+
+public class Chapter1_13 {
+
+	public static void main(String[] args) {
+/*
+ * Write a program that solves the following equation and displays the value for x and y:
+ * 3.4x + 50.2y = 44.5
+ * 2.1x + .55y = 5.9
+ */
+		double a = 3.4;
+		double b = 50.2;
+		double c =2.1;
+		double d = .55;
+		double e = 44.5;
+		double f =5.9;
+		double x;
+		double y;
+		
+		x = ((e*d - b*f)/(a*d - b*c));
+		y = ((a*f - e*c))/((a*d - b*c));
+		System.out.println("solve for the equation: \n3.4x + 50.2y = 44.5 \n2.1x + .55y = 5.9 \n");
+		System.out.println("X = " + x + "\nY = "+ y);
+	}
+
+}
