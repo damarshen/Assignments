8357f6728ba512744c984b3e5fb83bb5fd1c3c6a
ojall <ousainouj@gmail.com>
2017-09-02 04:32:46 -0400

    2_5

---
+++ src/Assignment1/Chapter2_05.java
@@ -0,0 +1,30 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class Chapter2_05 {
+
+	public static void main(String[] args) {
+		/*
+		 * Write a program that reads the subtotal and the gratuity rate, then computes the gratuity and total.
+		 */
+		
+		System.out.print("what is the subtotal?");
+		Scanner input= new Scanner (System.in);
+		double subtotal = input.nextDouble();
+		System.out.print("what is the Gratuity Rate?");
+		double gratuity = input.nextDouble();
+		
+		double gratuityRate = gratuity/100;
+		double gratuityOwed = gratuityRate * subtotal;
+		
+		double total = subtotal + (gratuityOwed);
+		
+		System.out.print("The gratuity is $" + gratuityOwed + " The total is $" + total );
+		
+		
+		
+		
+	}
+
+}
