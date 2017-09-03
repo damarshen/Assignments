44ce19f2acde6934a4c16c9e805ffe8940540f0e
ojall <ousainouj@gmail.com>
2017-09-02 23:00:55 -0400

    3_12

---
+++ src/Assignment1/Chapter3_12.java
@@ -0,0 +1,27 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+/*Write a program that prompts the user to enter a three-digit
+integer and determines whether it is a palindrome number.
+*/
+public class Chapter3_12 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a three-digit Number:");
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		int a = num/100 % 10;
+		int b = num/10 % 10; 
+		int c = num %10;
+		
+		if (a==c) {
+		System.out.println(num + " is a palindrome.");
+		}
+		else {
+			System.out.println(num + " is not a palindrome.");
+		}
+
+	}
+
+}
