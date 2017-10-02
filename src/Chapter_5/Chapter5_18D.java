c77ce809d46464153ccd019760cc4e2067c72e10
ojall <ousainouj@gmail.com>
2017-10-02 17:01:07 -0400

    5_18D

--- src/Chapter_5/Chapter5_18D.java
+++ src/Chapter_5/Chapter5_18D.java
@@ -3,7 +3,16 @@ package Chapter_5;
 public class Chapter5_18D {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Pattern D");
+		for (int i = 0; i < 6; i++){
+			for (int j = 0; j < i; j++){
+				System.out.print("  ");
+			}
+			for (int k = 0; k < 6 - i; k++){
+				System.out.print((k + 1) + " ");
+			}
+			System.out.println();
+		}
 
 	}
 
