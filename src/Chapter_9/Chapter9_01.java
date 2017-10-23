2fc44c7c54918be6d85330da6f3c120e8d565f7f
ojall <ousainouj@gmail.com>
2017-10-22 20:09:40 -0400

    9_01

---
+++ src/Chapter_9/Chapter9_01.java
@@ -0,0 +1,13 @@
+package Chapter_9;
+
+import Chapter_9.objectClasses.Rectangle;
+
+public class Chapter9_01 {
+	public static void main(String[] args) {
+		Rectangle R1 =new Rectangle(4, 40.0);
+		Rectangle R2 =new Rectangle(3.5, 35.9);
+		System.out.println("Rectangle 1 Width: " + R1.width+ ", Height: " + R1.height + ", Area: " + R1.getArea() + ", Perimiter: " + R1.getPerimiter());
+		System.out.println("Rectangle 2 Width: " + R2.width+ ", Height: " + R2.height + ", Area: " + R2.getArea() + ", Perimiter: " + R2.getPerimiter());
+		
+	}
+}
---
+++ src/Chapter_9/objectClasses/Rectangle.java
@@ -0,0 +1,23 @@
+package Chapter_9.objectClasses;
+
+public class Rectangle {
+		public double width = 1;
+		public double height =1;
+		
+		public Rectangle() {
+			width=1;
+			height=1;
+		}
+		public Rectangle(double a, double b) {
+			width=a;
+			height=b;
+		}
+		public double getArea() {
+			double area =width*height;
+			return area;
+		}
+		public double getPerimiter() {
+			double peremiter = (width*2)+(height*2);
+			return peremiter;
+		}
+}
